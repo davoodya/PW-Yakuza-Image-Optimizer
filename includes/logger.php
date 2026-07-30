4f226b7546bd75f1247e6d7fea4e62978faf8b90
davoodya <davoodya40@gmail.com>
2026-07-31 03:29:05 +0330

    Add includes/logger.php

---
+++ includes/logger.php
@@ -0,0 +1,48 @@
+﻿<?php
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+
+/*
+|--------------------------------------------------------------------------
+| YIO Logger
+|--------------------------------------------------------------------------
+*/
+
+
+function yio_log($message)
+{
+
+    if (!yio_get_option('logging', 1)) {
+
+        return;
+
+    }
+
+
+    $upload_dir = wp_upload_dir();
+
+
+    $log_file = $upload_dir['basedir'] . '/yio-debug.log';
+
+
+
+    $time = current_time('Y-m-d H:i:s');
+
+
+    $entry = sprintf(
+        "[%s] %s\n",
+        $time,
+        $message
+    );
+
+
+    file_put_contents(
+        $log_file,
+        $entry,
+        FILE_APPEND
+    );
+
+}
